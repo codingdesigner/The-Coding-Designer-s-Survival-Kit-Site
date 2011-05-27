6ef7f29a6d5d8d3eb81c3e9879b0098c7710dbcc
Mason Wendell <mason@zivtech.com>
2011-05-27 00:49:16 -0400

    working on presenting the feature descriptions

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@
 */
 
 
+$(document).ready(function () {
+  // $('nav.samples li a').hover(
+  //   function () {
+  //     console.log(this.hash);
+  //     console.log($(this));
+  //     console.log($(this).attr("href"));
+  //   }, 
+  //   function () {
+  //     
+  //   }
+  //   );
+  // console.log($('nav.samples li a'));
+});
+
+
 
 
 
