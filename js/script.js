5799acf0e3c7260342cb05a5c005e035c4cdecd6
Mason Wendell <mason@zivtech.com>
2011-05-27 11:26:48 -0400

    descriptions now show when you click on badges

--- js/script.js
+++ js/script.js
@@ -9,12 +9,14 @@ $(document).ready(function () {
   //     console.log(this.hash);
   //     console.log($(this));
   //     console.log($(this).attr("href"));
+  //     $(this).click();
   //   }, 
   //   function () {
   //     
   //   }
   //   );
   // console.log($('nav.samples li a'));
+  // $('#main article p').lettering('lines');
 });
 
 
