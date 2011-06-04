c18110877e1b802bce150c324397577c379a19cf
Mason Wendell <mason@zivtech.com>
2011-06-04 15:59:58 -0400

    tab nav for examples. unworking geared scrolling experiment
    Signed-off-by: Mason Wendell <mason@zivtech.com>

--- js/script.js
+++ js/script.js
@@ -2,19 +2,17 @@
 
 */
 
-
 $(document).ready(function () {
-  var tabContainers = $('#intro #teasers article');
-  tabContainers.hide().filter(':first').show();
-  
+  // teaser tabs
+  var teaserContainers = $('#intro #teasers article');
+  teaserContainers.hide().filter(':first').show();
   function badgeClick (el) {
-    tabContainers.hide().removeClass('selected');
-    tabContainers.filter(el.hash).show().addClass('selected');
+    teaserContainers.hide().removeClass('selected');
+    teaserContainers.filter(el.hash).show().addClass('selected');
     $('#intro nav.samples ul li a').removeClass('selected');
     $(el).addClass('selected');
     return false;
   }
-
   $('#intro nav.samples ul li a').click(function () {
     badgeClick(this);
     return false;
@@ -23,33 +21,42 @@ $(document).ready(function () {
     function () {
       badgeClick(this);
     });
-  //
+  
+  // code example tabs
+  var exampleContainers = $('#tools #tools-examples article');
+  exampleContainers.hide().filter(':first').show();
+  $('#tools nav.tabs ul li a').click(function () {
+    // tabClick(this);
+    // return false;
+    exampleContainers.hide().removeClass('selected');
+    exampleContainers.filter(this.hash).show().addClass('selected');
+    $('#tools nav.tabs ul li a').removeClass('selected');
+    $(this).addClass('selected');
+    return false;
+  }).filter(':first').click();
+    
+  // typography
   $('#intro #teasers article h1').lettering();
   $('#learn h1, #learn h2, #learn h3').lettering();
   
   // syntax highlighter
   SyntaxHighlighter.all();
+  
+  
+  //   // Geared Scrolling
+  //   "use strict";
+  //   var syncColumn;
+  //   console.log(syncColumn);
+  //   console.log('helllo');
+  //   $('#all-examples #tools-nav').addClass("column");
+  //   $('#all-examples #tools-examples').addClass("column");
+  //   $(window).load(function () {
+  //     console.log('hi mom');
+  //     // syncColumn = $('#all-examples').gearedscrolling();
+  //     
+  //     syncColumn = $('#all-examples').gearedscrolling();
+  //     // $('#all-examples').gearedscrolling();
+  //  console.log(syncColumn);
+  // });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
