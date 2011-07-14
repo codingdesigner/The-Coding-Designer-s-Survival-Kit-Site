163c3ac06b5cca24834e6d923d3ed2580fc53c55
Mason Wendell <mason@zivtech.com>
2011-07-14 10:10:40 -0400

    adding js
    Signed-off-by: Mason Wendell <mason@zivtech.com>

--- sites/all/themes/badge/template.php
+++ sites/all/themes/badge/template.php
@@ -1,5 +1,8 @@
 <?php
 
+drupal_add_js('js/Lettering.js');
+drupal_add_js('js/script.js');
+
 function badge_preprocess_page(&$variables) {
   $variables['site_name_a'] = "The Coding Designer&rsquo;s";
   $variables['site_name_b'] = "Survival Kit";
