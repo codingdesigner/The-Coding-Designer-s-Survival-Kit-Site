98c4ce4fe6169bc8f008eaa1abf6d3f9bcfb1f5d
Mason Wendell <mason@zivtech.com>
2011-07-14 23:49:23 -0400

    add .js
    Signed-off-by: Mason Wendell <mason@zivtech.com>

--- sites/all/themes/badge/template.php
+++ sites/all/themes/badge/template.php
@@ -1,9 +1,13 @@
 <?php
 
-drupal_add_js('js/Lettering.js');
-drupal_add_js('js/script.js');
+
 
 function badge_preprocess_page(&$variables) {
+// drupal_add_js(drupal_get_path('theme', 'MYTHEME') . '/mytheme.js', 'file');
+  drupal_add_js(drupal_get_path('theme', 'badge') . '/js/jquery.lettering.js');
+  drupal_add_js(drupal_get_path('theme', 'badge') . '/js/script.js');
+  
+  
   $variables['site_name_a'] = "The Coding Designer&rsquo;s";
   $variables['site_name_b'] = "Survival Kit";
 }
