0e08019aabbba8feea45677083adee23ae56ee8c
Mason Wendell <mason@zivtech.com>
2012-04-09 08:09:28 -0400

    rss feed, logo

--- sites/all/themes/badge/templates/page.tpl.php
+++ sites/all/themes/badge/templates/page.tpl.php
@@ -44,8 +44,10 @@
       <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
       <?php print render($title_suffix); ?>
 
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      <div class="content-wrapper">
+        <?php print $feed_icons; ?>
+        <?php print render($page['content']); ?>
+      </div>
     </div>
   </div> <!-- /main -->
 
